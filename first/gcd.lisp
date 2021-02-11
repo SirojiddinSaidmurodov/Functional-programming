9c97ee518349d6a8203cee95ba9146842a1c83d1
SirojiddinSaidmurodov <siroj.serj15@outlook.com>
2021-02-11 05:17:53 +0300

    add: added GCD function

---
+++ first/gcd.lisp
@@ -0,0 +1,26 @@
+;GCD function
+
+(DEFUN gcdE 
+    (m n) 
+    (IF 
+        (EQ n 0) m 
+        (gcdE n 
+            (mod m n))) )
+
+
+(defvar *input* 
+    (list :a 0 :b 0))
+
+(format t "Enter a: ")
+(setf 
+    (getf *input* :a)
+    (parse-integer 
+        (read-line)))
+(format t "Enter b: ")
+(setf 
+    (getf *input* :b)
+    (parse-integer 
+        (read-line)))
+
+(format t "The GCD of ~a and ~a is ~a" (getf *input* :a) (getf *input* :b) 
+    (gcdE (getf *input* :a) (getf *input* :b)))
