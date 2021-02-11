242b0775cd0efa64691355cc467504bbace5c7e2
SirojiddinSaidmurodov <siroj.serj15@outlook.com>
2021-02-11 04:31:54 +0300

    edit: detached factorial function

---
+++ first/factorial.lisp
@@ -0,0 +1,13 @@
+;Defining functions: e.g. factorial
+
+(DEFUN fact  
+    (n) 
+    (IF 
+        (EQ n 0) 1
+        (* n 
+            (fact  
+                (- n 1)))))
+
+(format t "Type the x ")
+(format t "Factorial of x is ~a" 
+    (fact (parse-integer (read-line))))
