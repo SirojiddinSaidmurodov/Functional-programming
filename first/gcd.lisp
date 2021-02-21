4c130b44cf685d1d065fd028de6dc4643f969958
SirojiddinSaidmurodov <siroj.serj15@outlook.com>
2021-02-21 16:13:32 +0300

    edit: code reformatting

--- first/gcd.lisp
+++ first/gcd.lisp
@@ -1,23 +1,13 @@
 ;GCD function
 
-(DEFUN gcdE 
-    (m n) 
-    (IF 
-        (EQ n 0) m 
-        (gcdE n 
-            (mod m n))) )
+(DEFUN gcdE (m n) 
+    (IF (EQ n 0)
+        m 
+        (gcdE n (mod m n))))
 
-(defun prompt-read 
-    (varname)
+(defun prompt-read (varname)
     (format t "Enter the ~a " varname)
-    (parse-integer 
-        (read-line)))
-(let 
-    (
-        (a 
-            (prompt-read "a") ) 
-        (b 
-            (prompt-read "b")))
+    (parse-integer (read-line)))
 
-    (format t "The GCD of ~a and ~a is ~a" a b
-        (gcdE a b)))
+(let ((a (prompt-read "a")) (b (prompt-read "b")))
+    (format t "The GCD of ~a and ~a is ~a" a b (gcdE a b)))
