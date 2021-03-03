f526c2d26b082abd701ef29608266ddd6ab5a6ea
SirojiddinSaidmurodov <siroj.serj15@outlook.com>
2021-03-03 18:13:15 +0300

    add: count function

---
+++ fourth/counta.lisp
@@ -0,0 +1,10 @@
+(defun counta (list)
+    (if (eq (cdr list) NIL)
+        (if (eq (car list) NIL)
+            0
+            1
+        )
+        (+ 1 (counta (cdr list)))
+    )
+)
+(format t "~a" (counta '(1 2 3 4 5 6 7)))
