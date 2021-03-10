ea27a90fe0f9334b956bff532a04730ffb76c4cc
SirojiddinSaidmurodov <siroj.serj15@outlook.com>
2021-03-10 18:35:29 +0300

    edit: made custom append function

--- fourth/invert.lisp
+++ fourth/invert.lisp
@@ -1,11 +1,21 @@
+(defun appendc (list_in1 list_in2)
+    (if (eq (cdr list_in1) NIL)
+        (if (eq (car list_in1) NIL)
+            list_in2
+            (cons (car list_in1) list_in2)
+        )
+        (cons (car list_in1) (appendc (cdr list_in1) list_in2))   
+    )
+)
 (defun invert (list_in) 
     (if (eq (cdr list_in) NIL)
         (if (eq (car list_in) NIL)
             NIL
             (list (car list_in))
         )
-        (append (invert (cdr list_in)) (list (car list_in)))
+        (appendc (invert (cdr list_in)) (list (car list_in)))
     )
 )
 
+(print (appendc `(1 2 3) `(4 5)))
 (print (invert `(1 2 3 4 5)))
