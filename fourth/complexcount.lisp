5d074e5abb0f1a019b0fae8889ef2a8cbea2baf2
SirojiddinSaidmurodov <siroj.serj15@outlook.com>
2021-03-10 17:50:49 +0300

    add: func for counting elements of non-atomic list

---
+++ fourth/complexcount.lisp
@@ -0,0 +1,11 @@
+(defun complexcount (input) 
+    (if (atom input)
+        (if (eq NIL input)
+            0
+            1
+        )
+        (+ (complexcount (car input)) (complexcount (cdr input)))
+    )
+)
+
+(print (complexcount `(1 ((2 3) 4) 5)))
