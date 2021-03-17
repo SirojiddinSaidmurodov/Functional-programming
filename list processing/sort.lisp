ad91ed8fc560c7b9dc572543423786038d757424
SirojiddinSaidmurodov <siroj.serj15@outlook.com>
2021-03-17 18:52:10 +0300

    refactor let and if clauses

--- list processing/sort.lisp
+++ list processing/sort.lisp
@@ -10,16 +10,11 @@
 
 (defun sort_list (input &optional desc)
     (if (eq (cdr input) NIL)
-        input
-        (if desc 
-            (let ((elem (maxx input)))
-                (cons elem (sort_list (rm_el input elem) desc))
-            )
-            (let ((elem (minn input)))
-                (cons elem (sort_list (rm_el input elem) desc))
-            )
+        input 
+        (let ((elem (if desc (maxx input) (minn input)) ))
+            (cons elem (sort_list (rm_el input elem) desc))
         )
     )
 )   
 
-(print ( sort_list (print (sort_list (print `(7 7 6 4 9 2 8 4 0 5 7 6)))) T) )
+(print (sort_list (print (sort_list (print `(7 7 6 4 9 2 8 4 0 5 7 6)))) T) )
