4a7989b2c269904b4db0cee7f3c1bc12c5784ed8
SirojiddinSaidmurodov <siroj.serj15@outlook.com>
2021-03-17 18:45:29 +0300

    edit sort for ascending and descending order

--- list processing/sort.lisp
+++ list processing/sort.lisp
@@ -1,4 +1,5 @@
 (load "min.lisp")
+(load "maxx.lisp")
 
 (defun rm_el (input el)
     (if (eq (car input) el)
@@ -7,13 +8,18 @@
     )
 )
 
-(defun sort_list (input)
+(defun sort_list (input &optional desc)
     (if (eq (cdr input) NIL)
         input
-        (let ((cur_min (minn input)))
-            (cons cur_min (sort_list (rm_el input cur_min)))
+        (if desc 
+            (let ((elem (maxx input)))
+                (cons elem (sort_list (rm_el input elem) desc))
+            )
+            (let ((elem (minn input)))
+                (cons elem (sort_list (rm_el input elem) desc))
+            )
         )
     )
 )   
 
-(print (sort_list (print `(7 7 6 4 9 2 8 4 0 5 7 6)))) 
+(print ( sort_list (print (sort_list (print `(7 7 6 4 9 2 8 4 0 5 7 6)))) T) )
